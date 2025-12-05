64a69ecf893e60cc68c5d17bb4e376af2e76217c
GioEvaristo <giiofonseca08@gmail.com>
2025-12-05 17:10:52 -0300

    05/12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,47 +1,46 @@
 @extends('layout')
 @section('content')
 <div class="container com-margem">
-    <div class="row">
+    <div class="noticias-grid">
 
         @foreach ($noticias as $noticia)
-        <div class="col-md-3 mb-3">
-            <div class="card mb-4 shadow-sm noticiaCard">
+        <div class="card mb-2 shadow-sm noticiaCard">
 
-                <div class="noticiaheader">
-                    <h3>{{ $noticia->titulo }}</h3>
-                    <p>{{ $noticia->created_at->format('d/m/Y') }}</p>
-                </div>
+            <div class="noticiaheader">
+                <h3>{{ $noticia->titulo }}</h3>
+                <p>{{ $noticia->created_at->format('d/m/Y') }}</p>
+            </div>
 
-                @if ($noticia->imagem)
-                <img src="{{ asset('storage/' . $noticia->imagem) }}" class="card-img-top" style="height: 200px; object-fit: cover;">
-                @endif
+            @if ($noticia->imagem)
+            <img src="{{ asset('storage/' . $noticia->imagem) }}" class="card-img-top" alt="{{ $noticia->titulo }}">
+            @endif
 
-                <div class="card-body text-center noticiadescricao">
-                    <p class="textnoticia">{{ Str::limit($noticia->descricao, 120) }}</p>
+            <div class="card-body text-center noticiadescricao">
+                <p class="textnoticia">{{ Str::limit($noticia->descricao, 120) }}</p>
 
-                    <button type="button" class="btn btn-secondary" 
-                            data-bs-toggle="modal" 
-                            data-bs-target="#modal{{ $noticia->id }}">
-                        Ver Mais
-                    </button>
-                </div>
+                <button type="button" class="btn btn-secondary" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#modal{{ $noticia->id }}">
+                    Ver Mais
+                </button>
             </div>
         </div>
 
-        <div class="modal fade" id="modal{{ $noticia->id }}" tabindex="-1">
+        <!-- Modal -->
+        <div class="modal fade" id="modal{{ $noticia->id }}" tabindex="-1" aria-labelledby="modalLabel{{ $noticia->id }}" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">{{ $noticia->titulo }}</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        <h5 class="modal-title" id="modalLabel{{ $noticia->id }}">{{ $noticia->titulo }}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
                     </div>
 
                     <div class="modal-body">
                         @if ($noticia->imagem)
-                        <img src="{{ asset('storage/' . $noticia->imagem) }}" class="img-fluid mb-3">
+                        <img src="{{ asset('storage/' . $noticia->imagem) }}" class="img-fluid mb-3" alt="{{ $noticia->titulo }}">
                         @endif
                         <p>{{ $noticia->descricao }}</p>
-                        <p>Responsável pela notícia: {{ $noticia->autor }}</p>
+                        <p><strong>Responsável pela notícia:</strong> {{ $noticia->autor }}</p>
                     </div>
                 </div>
             </div>
@@ -52,4 +51,3 @@
 </div>
 
 @endsection
-
